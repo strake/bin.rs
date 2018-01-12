49bc12cf27895fd3d5250cccbc72c99570b4b319
M Farkas-Dyck <strake888@gmail.com>
2018-01-12 13:37:20 -0500

    Start

---
+++ src/slice.rs
@@ -0,0 +1,27 @@
+use core::mem;
+
+pub struct Bits([u8]);
+
+impl<'a> From<&'a [u8]> for &'a Bits {
+    #[inline]
+    fn from(bs: &'a [u8]) -> Self { unsafe { mem::transmute(bs) } }
+}
+
+impl<'a> From<&'a mut [u8]> for &'a mut Bits {
+    #[inline]
+    fn from(bs: &'a mut [u8]) -> Self { unsafe { mem::transmute(bs) } }
+}
+
+pub struct Iter<'a> {
+    bits: &'a Bits,
+    pos: usize,
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = bool;
+    #[inline]
+    fn next(&mut self) -> Option<bool> {
+        let (m, n) = (self.pos & 7, self.pos >> 3);
+        self.bits.0.get(n).map(|x| 0 != x & (1 << m))
+    }
+}
